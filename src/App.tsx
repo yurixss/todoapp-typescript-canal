87f06d658183a5e8f83db494f66321655b41db7f
yurixss <yurinuness777@gmail.com>
2023-01-16 20:16:12 -0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import TodoTask from "./components/TodoTask/TodoTask";
 
 import './styles/styles.css'
 
 function App() {
+
+	const 
+	[task, setTask] = useState<string>(""),
+	[todoList, setTodoList] = useState([])
+
+	interface ITask {
+		id: number,
+		nameTask: string
+	}
 
 	return (
 		<div className="App">
@@ -16,6 +26,8 @@ function App() {
 					placeholder="Escrever task..." 
 					name="task"
 					className="input"
+					value={task}
+					onChange={(event) => setTask(event.target.value)}
 				/>
 
 				<button type="submit" className="btn-header">Adicionar Task</button>
